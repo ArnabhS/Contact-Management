c60d00e01f13c499f4471ee03ed50aa30285af43
ArnabhS <sinhaarnabh888@gmail.com>
2024-11-16 00:48:49 +0530

    update: add .env to .gitignore and use environment variable for API URL in service

--- client/src/services/service.js
+++ client/src/services/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/contacts';
+const API_URL = import.meta.env.API_URL;
 
 export const fetchContacts = async () => {
     const response = await axios.get(API_URL);
